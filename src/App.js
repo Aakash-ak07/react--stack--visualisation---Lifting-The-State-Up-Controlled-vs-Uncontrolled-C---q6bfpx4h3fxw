a1705e7300e8fe9425c5bf72737e2cea2931a697
Aakash <111000108+Aakash-ak07@users.noreply.github.com>
2023-01-22 00:11:48 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import './styles/App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import React from 'react';
+import React, { useState } from 'react';
 import Element from './components/element';
 
 function App() {
-  const[text, setText] = useState();
-  const[arr, setArr] = useState([]);
-  const[popEle, setPopELe] = useState('');
-  const[topELe, setTopEle] = useState('');
+
+  const [text, setText] = useState();
+  const [arr, setArr] = useState([]);
+  const [popEl, setPopEle] = useState('')
+  const [topEl, setTopEle] = useState('')
 
   const handleInput = (e) => {
     setText(e.target.value);
   }
 
   const handlePush = () => {
-    setArr(text, [...arr])
+    setArr([text,...arr]);
   }
 
   const handlePop = () => {
@@ -31,41 +32,42 @@ function App() {
       window.alert('Stack is empty')
     }
     else{
-      setTopEle(arr.shift())
+      setTopEle(arr[0])
     }
   }
-  
+
   return (
     <div className="container">
       <h1 className="header">Stack</h1>
       <div className="row">
         <div className="col md-6 stack">
-          <Element />
+          {arr.map((item,index)=>  <Element key={index} item={item} />)}
         </div>
         <div className="col md-3 operation">
           <h2>Operations</h2>
           <div className="input-group push">
-            <button className="btn btn-outline-secondary" type="button" id="button-addon2">
+            <button className="btn btn-outline-secondary" type="button" id="button-addon2" onClick={handlePush}>
               PUSH
             </button>
-            <input type="text" className="form-control" />
+            <input type="text" className="form-control" onChange={handleInput}/>
           </div>
           <div className="input-group pop">
-            <button className="btn btn-outline-secondary" type="button" id="button-addon2">
+            <button className="btn btn-outline-secondary" type="button" id="button-addon2" onClick={handlePop}>
               POP
             </button>
-            <input type="text" disabled className="form-control" />
+            <input type="text" disabled className="form-control" value={popEl} />
           </div>
           <div className="input-group top">
-            <button className="btn btn-outline-secondary" type="button" id="button-addon2">
+            <button className="btn btn-outline-secondary" type="button" id="button-addon2" onClick={handleTop}>
               TOP
             </button>
-            <input type="text" disabled className="form-control" />
+            <input type="text" disabled className="form-control" value={topEl}/>
           </div>
         </div>
       </div>
     </div>
   );
 }
+
 
 export default App;
